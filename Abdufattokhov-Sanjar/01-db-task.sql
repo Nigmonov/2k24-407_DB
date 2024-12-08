92027063f795ba6081fc4735bd196ab94e7403ea
Sanjar <104901089+abstract000@users.noreply.github.com>
2024-12-08 17:01:18 +0900

    Update 01-db-task.sql

--- Abdufattokhov-Sanjar/01-db-task.sql
+++ Abdufattokhov-Sanjar/01-db-task.sql
@@ -104,3 +104,27 @@ INSERT INTO enrollments (student_id, course_id, grade)
 VALUES (
     (SELECT student_id FROM students WHERE first_name = 'Sanjar' AND last_name = 'Abdufattokhov'), 1, 5
 );
+
+SELECT 
+    s.first_name, 
+    s.last_name, 
+    COUNT(e.course_id) AS total_courses
+FROM 
+    students s
+JOIN 
+    enrollments e ON s.student_id = e.student_id
+GROUP BY 
+    s.student_id, s.first_name, s.last_name
+HAVING 
+    COUNT(e.course_id) > (
+        SELECT 
+            AVG(course_count) 
+        FROM (
+            SELECT 
+                COUNT(course_id) AS course_count
+            FROM 
+                enrollments
+            GROUP BY 
+                student_id
+        ) AS avg_courses
+    );
